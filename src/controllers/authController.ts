b284fad4f6929b7e17b0592f5290e5165dbf6770
Positive-Conative <kyj970513@naver.com>
2019-05-10 05:13:00 +0900

    [Modify] Auth dto 부분 수정

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,12 +2,14 @@ import { Request, Response, NextFunction } from 'express';
 import chkChar from '../lib/chkChar';
 import { userRepo } from '../models/repository/userRepo';
 
-import { addAuthDto, setAuthFlagDto, modifyAuthDto } from '../interfaces/authDto';
+import {
+    addAuthDto
+} from '../interfaces/authDto';
 
 const aRepo = new userRepo();
 const authController = {
     // 유저 추가
-     addUser : async (req: Request, res: Response, next: NextFunction) => {
+    addUser: async (req: Request, res: Response, next: NextFunction) => {
         const bodyData: addAuthDto = {
             u_email: req.body.userId,
             u_password: req.body.userPw,
@@ -16,87 +18,87 @@ const authController = {
             u_flag: 0,
             u_permission: 'default',
         }
-        
+
         // 파라미터 Check
-        if(chkChar(bodyData) === false || bodyData.u_email.indexOf('@') === -1) {
+        if (chkChar(bodyData) === false || bodyData.u_email.indexOf('@') === -1) {
             return next('API002');
         }
 
         // 유저 중복 여부 확인 (중복 확인)
-        if(await aRepo.findUserOne(bodyData.u_email)) {
+        if (await aRepo.findUserOne(bodyData.u_email)) {
             return next('API100');
         }
 
         await aRepo.addUser(bodyData);
-        res.json({"message": "처리 완료!"});
+        res.json({ "message": "처리 완료!" });
     },
 
-    getUserInfo: async(req: Request, res: Response, next: NextFunction) => {
-        
-        const u_email = req.body.userId;
+    // getUserInfo: async(req: Request, res: Response, next: NextFunction) => {
 
-         // 잘못된 파라미터?
-        if(chkChar(u_email) === false) {
-            return next('API002');
-        }
+    //     const u_email = req.body.userId;
 
-        const result = await aRepo.findUserOne(u_email);
-        if(result === undefined) {
-            return next('API200');
-        }
+    //      // 잘못된 파라미터?
+    //     if(chkChar(u_email) === false) {
+    //         return next('API002');
+    //     }
 
-        // 비공개 유저인지 확인하고, 정상이라면 flag 지움 처리
-        if(result.flag === 1) {
-            return next('API300');
-        } else {
-            delete result.flag;
-        }
+    //     const result = await aRepo.findUserOne(u_email);
+    //     if(result === undefined) {
+    //         return next('API200');
+    //     }
+
+    //     // 비공개 유저인지 확인하고, 정상이라면 flag 지움 처리
+    //     if(result.flag === 1) {
+    //         return next('API300');
+    //     } else {
+    //         delete result.flag;
+    //     }
 
 
-        res.json({result, "message": "정상적으로 조회되었습니다."});
-    },
- 
-    setUserFlag: async (req: Request, res: Response, next: NextFunction) => {
-        const bodyData : setAuthFlagDto = {
-            u_email: req.body.userId,
-            u_flag: req.body.state,
-        }
+    //     res.json({result, "message": "정상적으로 조회되었습니다."});
+    // },
 
-        if(chkChar(bodyData) === false || bodyData.u_flag in [0, 1, 2] === false ) {
-            return next('API002');
-        }
+    // setUserFlag: async (req: Request, res: Response, next: NextFunction) => {
+    //     const bodyData : setAuthFlagDto = {
+    //         u_email: req.body.userId,
+    //         u_flag: req.body.state,
+    //     }
 
-        await aRepo.modifyUserFlag(bodyData);
-        res.json({"message": "정상적으로 처리되었습니다."});        
-    },
+    //     if(chkChar(bodyData) === false || bodyData.u_flag in [0, 1, 2] === false ) {
+    //         return next('API002');
+    //     }
 
-    modifyUser: async (req: Request, res: Response, next: NextFunction) => {
-        const bodyData: modifyAuthDto = {
-            u_email: req.body.userId,
-            u_password: req.body.userPw,
-            u_name: req.body.userName,
-            u_nickname: req.body.userNick,
-        }
+    //     await aRepo.modifyUserFlag(bodyData);
+    //     res.json({"message": "정상적으로 처리되었습니다."});        
+    // },
 
-        // 파라미터 Check
-        if(chkChar(bodyData) === false || bodyData.u_email.indexOf('@') === -1) {
-            return next('API002');
-        }
+    // modifyUser: async (req: Request, res: Response, next: NextFunction) => {
+    //     const bodyData: modifyAuthDto = {
+    //         u_email: req.body.userId,
+    //         u_password: req.body.userPw,
+    //         u_name: req.body.userName,
+    //         u_nickname: req.body.userNick,
+    //     }
 
-        if(await aRepo.findUserOne(bodyData.u_email) === undefined) {
-            return next('API200');
-        }
+    //     // 파라미터 Check
+    //     if(chkChar(bodyData) === false || bodyData.u_email.indexOf('@') === -1) {
+    //         return next('API002');
+    //     }
 
-        // 유저 존재 여부 확인 (중복 확인)
-        const result = await aRepo.findUserOne(bodyData.u_email);
-        if(result === undefined) {
-            return next('API100');
-        }
-        
-        await aRepo.modifyUserInfo(bodyData);
-        res.json({"message": "정상적으로 처리되었습니다."});
-        
-    },
+    //     if(await aRepo.findUserOne(bodyData.u_email) === undefined) {
+    //         return next('API200');
+    //     }
+
+    //     // 유저 존재 여부 확인 (중복 확인)
+    //     const result = await aRepo.findUserOne(bodyData.u_email);
+    //     if(result === undefined) {
+    //         return next('API100');
+    //     }
+
+    //     await aRepo.modifyUserInfo(bodyData);
+    //     res.json({"message": "정상적으로 처리되었습니다."});
+
+    // },
 }
 
 
